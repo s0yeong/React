64c9aee08e7cab0a6568478a3fb1e0d5699797bb
seo <deergirl11@gmail.com>
2023-02-17 02:04:29 +0900

    use useEffect function

---
+++ react-app-for-beginner/src/App.js
@@ -0,0 +1,33 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
